943cde9cbf828e4b0a277949d92c6abfa53c53df
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-20 09:08:06 +0600

    debug_true

--- Freelancer_managents/settings.py
+++ Freelancer_managents/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9s0g_@%@0q+rs(eu8u4^0*2*h7uzdr&oqnw0mh61xo@(qs+%#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app",'127.0.0.1']
 
