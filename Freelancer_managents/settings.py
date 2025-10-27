fc4c6c3825c0136b08b923b623c09ec6cfbf0fba
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-27 22:54:20 +0600

    refreshTokenOf

--- Freelancer_managents/settings.py
+++ Freelancer_managents/settings.py
@@ -200,7 +200,7 @@ REST_FRAMEWORK={
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
    'ACCESS_TOKEN_LIFETIME':timedelta(days=100),
-   "REFRESH_TOKEN_LIFETIME": timedelta(days=100),
+#    "REFRESH_TOKEN_LIFETIME": timedelta(days=100),
 }
 
 DJOSER = {
