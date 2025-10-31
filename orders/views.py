8610d2138327d7ee760dedad841279aeae7c9425
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-31 20:48:59 +0600

    gpt code2

--- orders/views.py
+++ orders/views.py
@@ -197,14 +197,6 @@ class ServiceOrderViewSet(ModelViewSet):
 # def payment_fail(request):
 #     return HttpResponseRedirect(f"{main_setting.FRONTEND_URL}/dashboard/orders/")
 
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from orders.models import ServiceOrder
-from django.conf import settings as main_setting
-from sslcommerz_lib import SSLCOMMERZ
-
 @api_view(['POST'])
 def initiate_payment(request):
     user = request.user
